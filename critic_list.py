9b4a5b5271351771e82af580b43484e66b63ee28
oxtay <okhtay@gmail.com>
2014-05-20 23:17:09 -0700

    skeleton of the app, review scraping complete

--- critic_list.py
+++ critic_list.py
@@ -4,25 +4,51 @@ import requests
 import re
 import os
 import sys
+import nltk
 
 
 REVIEWERS = '/externalreviews'
 IMDB_PAGE = 'http://www.imdb.com'
-tmp_path = '/title/tt0109830' # For Forrest Gump
+tmp_path  = '/title/tt0109830' # For Forrest Gump
+
 
 def get_ciritic_list(movie_url):
-	# Extracts the list of external reviews from a movie's page on IMDB
-	
-	html = urllib2.urlopen( movie_url ).read()
-	soup = bs(html)
-	div = soup.find('div', {'id':'external_reviews_content'})
+# Extracts the list of external reviews from a movie's page on IMDB
+	html 		= urllib2.urlopen( movie_url ).read()
+	soup_movie 	= bs(html)
+	div 		= soup_movie.find('div', {'id':'external_reviews_content'})
 	external_urls = [IMDB_PAGE + a['href'] for a in div.find_all('a', href = True) if a]
-	for row in external_urls:
-		print 'URL:', row
-	print len(external_urls)
-	sys.exit(0)
+	return external_urls
 
-def go_to_url(url_list):
+def get_review_in_url(review_url):
+# Having a URL as the input, extracts the content of the review from that URL
+	review_html 	= urllib2.urlopen( review_url ).read()
+	soup_review 	= bs(review_html)
+	texts 	= soup_review.find_all(text = True)
+
+	# Time it to see which one of these are faster:
+	# Solution 1 
+	review_content = nltk.clean_html(texts)
+
+	# Solution 2
+	review_content = filter(visible, texts)
+
+	# Exit here for debugging
+	sys.exit(0)
+	return review_content
+
+def visible(element):
+# Used in get_review_in_url fcn
+
+    if element.parent.name in ['style', 'script', '[document]', 'head', 'title']:
+        return False
+    elif re.match('<!--.*-->', str(element)):
+        return False
+    return True
+
+ 
+def get_review_sentiment(text):
+# using nltk, it gives a measure for the sentiment of a movie review
 	pass
 
 def main():
